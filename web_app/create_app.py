435064c105a6a9252970c91a6b461ae9a05a6ec9
Danil1994 <danilaleksandrovich36@gmail.com>
2023-07-26 08:37:43 +0300

    change swagger

--- web_app/create_app.py
+++ web_app/create_app.py
@@ -11,12 +11,12 @@ app = Flask(__name__)
 api = Api(app)
 
 api.add_resource(HelloWorld, '/')
-api.add_resource(FindGroupsWithStudentCount, '/api/v1/groups')
-api.add_resource(FindStudentsRelatedToTheCourse, '/api/v1/students')
-api.add_resource(CreateStudent, '/api/v1/students')
-api.add_resource(DeleteStudent, '/api/v1/students/<student_id>')
-api.add_resource(AddStudentToTheCourse, '/api/v1/students/<student_id>/courses/<course_id>')
-api.add_resource(RemoveStudentFromCourse, '/api/v1/student/<student_id>/courses/<course_id>')
+api.add_resource(FindGroupsWithStudentCount, '/api/v1/groups', methods=['GET'])
+api.add_resource(FindStudentsRelatedToTheCourse, '/api/v1/students/courses', methods=['GET'])
+api.add_resource(CreateStudent, '/api/v1/students', methods=['POST'])
+api.add_resource(DeleteStudent, '/api/v1/students/<student_id>', methods=['DELETE'])
+api.add_resource(AddStudentToTheCourse, '/api/v1/students/<student_id>/courses/<course_id>', methods=['PATCH'])
+api.add_resource(RemoveStudentFromCourse, '/api/v1/student/<student_id>/courses/<course_id>', methods=['DELETE'])
 
 swagger = Swagger(app)
 
